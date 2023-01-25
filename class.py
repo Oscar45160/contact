a2e070905e80038a14bd4485ee395a54120b97b7
Omicron45 <omicron.pimpaud@gmail.com>
2023-01-25 16:02:12 +0100

    added class.py

---
+++ class.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+class Contact:
+    def __init__(self, first_name, last_name, phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.creation_time = datetime.datetime.now()
+        self.updated_phone = None
+
+    def update_phone(self, new_phone):
+        self.updated_phone = self.phone
+        self.phone = new_phone
