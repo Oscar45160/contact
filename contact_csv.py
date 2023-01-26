398f29d6f7962b62ced56f1cc592750fc089acfa
Omicron45 <omicron.pimpaud@gmail.com>
2023-01-26 09:33:52 +0100

    updated contact_csv.py

--- contact_csv.py
+++ contact_csv.py
@@ -1,27 +1,29 @@
-from datetime import date
 import pandas as pd
+from datetime import datetime
+import os
 
-list_first_name = []
-list_last_name = []
-list_phone_number = []
-list_creation_date = []
 
-first_name = input("First Name : \n")
-list_first_name.append(first_name)
-last_name = input("Last Name : \n")
-list_last_name.append(last_name)
-phone_number = input("Phone Number : \n")
-list_phone_number.append(phone_number)
-creation_date = date.today()
-list_creation_date.append(creation_date)
+def create_contact():
+    first_name = input("Veuillez entrer le prénom :")
+    last_name = input("Veuillez entrer le nom :")
+    phone = input("Veuillez entrer le numéro de mobile :")
+    creation_date = datetime.today().date()
+    dict_contact = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone": phone,
+        "creation_date": creation_date
+    }
 
-data = {}
-data['First Name'] = list_first_name
-data['Last Name'] = list_last_name
-data['Phone Number'] = list_phone_number
-data['Creation Date'] = list_creation_date
-data_frame = pd.DataFrame(data)
+    # convert the dictionary to a DataFrame
+    df_contact = pd.DataFrame(dict_contact, index=[0])
+    # if the file doesn't exist, create it and add the header
+    if not os.path.isfile("contact.csv"):
+        df_contact.to_csv("contact.csv", header=True, index=False)
+    else:  # else it exists so append without writing the header
+        df_contact.to_csv("contact.csv", mode='a', header=False, index=False)
+    return dict_contact
 
-data_frame.to_csv('contact.csv', index=False)
-contact_csv = pd.read_csv('contact.csv')
-print(contact_csv)
+
+dic = create_contact()
+print(dic)
